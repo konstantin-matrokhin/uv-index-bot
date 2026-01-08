7af3b3cf66689d09c8afc50021b7d3bfa07e9eed
Konstantin Matrokhin <konstantin.matrokhin@gmail.com>
2026-01-08 18:54:16 +0100

    fix compose files

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,11 @@
 set -e
 set -x
 
-docker buildx build --no-cache --platform=linux/amd64 -t ghcr.io/konstantin-matrokhin/uv-index-bot:latest --push .
+docker buildx create --use --name multi || true
+docker buildx inspect --bootstrap
+
+docker buildx build \
+  --no-cache \
+  --platform=linux/amd64,linux/arm64 \
+  -t ghcr.io/konstantin-matrokhin/uv-index-bot:latest \
+  --push .
